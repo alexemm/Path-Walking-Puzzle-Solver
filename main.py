72d1ef71a65595883cf583967953e3fa6872e4a0
Alexander Emmerich <al_emmerich@gmx.de>
2022-07-03 17:13:29 +0200

    Added error messages for erroneous instance files

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
-import networkx as nx
 import matplotlib.pyplot as plt
+import networkx as nx
 
-from models.dynamic import hamilton
+from models.dynamic import hamilton2
 
 
 def read_txt(filename):
     with open(filename) as f:
         lines = f.read().splitlines()
-    starting_point = [(int(line.split()[1]), int(line.split()[2])) for line in lines if line.startswith("S")]
-    non_walk_points = [(int(line.split()[1]), int(line.split()[2])) for line in lines if line.startswith("N")]
-    walk_points = [(int(line.split()[1]), int(line.split()[2])) for line in lines if line.startswith("W")]
+    try:
+        starting_point = [(int(line.split()[1]), int(line.split()[2])) for line in lines if line.startswith("S")]
+        non_walk_points = [(int(line.split()[1]), int(line.split()[2])) for line in lines if line.startswith("N")]
+        walk_points = [(int(line.split()[1]), int(line.split()[2])) for line in lines if line.startswith("W")]
+        finishing_point = [(int(line.split()[1]), int(line.split()[2])) for line in lines if line.startswith("F")]
+    except IndexError:
+        print("Lines in file is not structured as (S|W|N|F) x y\n"
+              "Please change this and try again!")
+        exit(1)
+    return starting_point, non_walk_points, walk_points, finishing_point
 
-    return starting_point, non_walk_points, walk_points
 
-
-def check_for_correctness(starting_point, non_walk_points, walk_points):
-    assert len(starting_point) <= 1
-    assert len(non_walk_points) * len(walk_points) == 0 and (len(non_walk_points) > 0 or len(walk_points) > 0)
+def check_for_correctness(starting_point, non_walk_points, walk_points, finishing_point):
+    try:
+        assert len(starting_point) <= 1
+        assert len(finishing_point) <= 1
+        assert len(non_walk_points) * len(walk_points) == 0 and (len(non_walk_points) > 0 or len(walk_points) > 0)
+    except AssertionError:
+        print(
+            "Either, you have more than one starting point or you use both, walking points and non-walking points in your instance.\n"
+            "Please change that and try again!")
+        exit(1)
 
 
 def create_grid_graph(walkable_nodes):
@@ -51,32 +63,46 @@ def plot_grid(g, ham_path=None):
     nx.draw(g, pos=pos,
             node_color='lightgreen',
             edge_color=colors,
-            #with_labels=True,
+            # with_labels=True,
             node_size=600)
     plt.show()
 
 
-def setup_graph(filename: str):
-    starting_point, non_walk_points, walk_points = read_txt(filename)
-    check_for_correctness(starting_point, non_walk_points, walk_points)
+def setup_graph(starting_point, non_walk_points, walk_points, finishing_point):
+
+    check_for_correctness(starting_point, non_walk_points, walk_points, finishing_point)
 
     max_x = max(t[0] for t in non_walk_points)
     max_y = max(t[1] for t in non_walk_points)
 
     g = nx.grid_2d_graph(max_x + 1, max_y + 1)
     g.remove_nodes_from(non_walk_points)
-
+    starting_point = starting_point[0]
+    try:
+        assert starting_point in g.nodes
+        if len(finishing_point) != 0:
+            finishing_point = finishing_point[0]
+            assert finishing_point in walk_points
+    except AssertionError:
+        print("Starting/finishing point not in graph!")
+        exit(1)
     return g
 
 
 def get_neighbor_representation(g):
-    return {node: list(g.neighbors(node)) for node in g.nodes}
+    return {node: set(g.neighbors(node)) for node in g.nodes}
+
+
+def solve(filename):
+    starting_point, non_walk_points, walk_points, finishing_points = read_txt(filename)
+    g = setup_graph(starting_point, non_walk_points, walk_points, finishing_points)
+
+    solution = hamilton2(get_neighbor_representation(g), (5, 0))
+    # solution = hamilton(get_neighbor_representation(g), len(g.nodes), (5,0))
+    print(solution)
+    plot_grid(g, solution)
 
 
 if __name__ == '__main__':
-    g = setup_graph("instances/instance1")
+    solve("instances/instance1")
 
-    #solution = hamilton(get_neighbor_representation(g), (5, 0))
-    solution = hamilton(get_neighbor_representation(g), len(g.nodes), (5,0))
-    print(solution)
-    plot_grid(g, solution)
